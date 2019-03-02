258326205c0726e883d262317f9bd7bd46fb5bec
Chelsey McKinney <chelseymckinney@gmail.com>
2019-03-01 20:23:08 -0600

    current push of js code

--- tiny-app/js/app.js
+++ tiny-app/js/app.js
@@ -10,4 +10,42 @@
     storageBucket: "small-app-33cef.appspot.com",
     messagingSenderId: "14412103389"
   };
-  firebase.initializeApp(config);
+	firebase.initializeApp(config);
+	
+	//reference database
+	var database = firebase.database();
+
+	//capture button click
+	//button will recognize click + increment the hit counts
+	$('.create-hit-url').on("click", function(){
+
+		//use event.preventDefault to stop form?
+		//possibly would cause hit count to increment
+	})
+
+	//grab value from URL input
+	var formInput = $('.form-input').val().trim();
+
+
+
+	//code for handling push
+	database.ref().push({
+		formInput: formInput,
+	
+})
+//checking in console for information
+console.log(formInput);
+
+
+//Firebase watcher + initial loader 
+    //code that works the same as .on("value")
+    database.ref().on("child_added", function(childSnapshot) {
+			//console log everything from childSnapshot to double check
+			
+
+			//Change HTML to reflect information
+			$(".round-tbl").append('<tr><td>' + variable + '</td><td>' + variable + '</td><td>' + variable + '</td><td>' + variable + '</td></tr>');
+
+			//syntax--id.text(childsnapshot) value. variable name 
+			$("#id-name").text(childSnapshot.val().variable);
+		} 
