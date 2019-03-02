b5b19ff4d9231a30190ceb8811d29ebe912b2bcb
Chelsey McKinney <chelseymckinney@gmail.com>
2019-03-01 20:04:36 -0600

    creating js folder/files

---
+++ tiny-app/js/app.js
@@ -0,0 +1,13 @@
+//still need link on line 2?
+{/* <script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script> */}
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBI7ldhUIltYYlG2yUYQIabAkQGWCxzCwE",
+    authDomain: "small-app-33cef.firebaseapp.com",
+    databaseURL: "https://small-app-33cef.firebaseio.com",
+    projectId: "small-app-33cef",
+    storageBucket: "small-app-33cef.appspot.com",
+    messagingSenderId: "14412103389"
+  };
+  firebase.initializeApp(config);
